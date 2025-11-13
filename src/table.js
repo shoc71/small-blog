29e97301f68f3fb072d82c4fe042a27ba11bb729
shoc71 <supersonicwind6@gmail.com>
2025-11-12 23:15:41 -0500

    MVP

--- src/table.js
+++ src/table.js
@@ -1,66 +1,52 @@
-const displayTable = document.querySelector("#displayTable tbody");
-const orderContentButton = document.querySelector("#order");
-const searchInput = document.getElementById("searchInput");
-const rows = document.querySelectorAll("#displayTable tbody tr")
-let reverseOrderButtonClicked = false;
-
-const posts = {
-    Intelligence: `<a href="./posts/intelligence.html">Intelligence</a>`,
-    Smart_Chars: `<a href="./posts/smart-chars.html">Smart Characters</a>`,
-    Social_Ladder: `<a href="./posts/social-status.html">Social Status</a>`
-}
-
-const tableContents = [
-    { id: 1, link: posts.Intelligence, date:"2025-10-20", tags: ["Analysis", "INCOMPLETE"]},
-    { id: 2, link: posts.Smart_Chars, date:"2025-10-22", tags: ["Hot Take", "INCOMPLETE"]},
-    { id: 3, link: posts.Social_Ladder, date:"2025-11-11", tags: ["New", "INCOMPLETE"]},
-]
-
-function renderReversedTable() {
-    return tableContents.reverse()
-}
-
-let display = reverseOrderButtonClicked ? tableContents : renderReversedTable();
-
-// mouse clicks
-orderContentButton.addEventListener("click", () => {
-    reverseOrderButtonClicked = !reverseOrderButtonClicked
-
-    displayTable.innerHTML = display
-        .map(rowContent => `
-        <tr>
-            <td>${rowContent.id}</td>
-            <td>${rowContent.link}</td>
-            <td>${rowContent.date}</td>
-            <td>${rowContent.related}</td>
-            <td>${rowContent.tags}</td>
-        </tr>
-    `).join("");
-});
-
-// 
-searchInput.addEventListener('input', function() {
-    const filter = searchInput.value.toLowerCase()
-
-    console.log(filter)
-
-    rows.forEach(row=> {
-        const text = row.textContent.toLocaleLowerCase();
-
-        row.classList.toggle('hidden', !text.includes(filter))
-    })
-});
-
 // window init
 window.addEventListener("DOMContentLoaded", () => {
-    displayTable.innerHTML = display
-        .map(rowContent => `
-        <tr>
-            <td>${rowContent.id}</td>
-            <td>${rowContent.link}</td>
-            <td>${rowContent.date}</td>
-            <td>${rowContent.related}</td>
-            <td>${rowContent.tags}</td>
-        </tr>
-    `).join("");
+    const displayTable = document.querySelector("#displayTable tbody");
+    const orderContentButton = document.querySelector("#order");
+    const searchInput = document.getElementById("searchInput");
+    let reverseOrderButtonClicked = false;
+
+    const posts = {
+        Intelligence: `<a href="./posts/intelligence.html" class="link-white-hover">Intelligence</a>`,
+        Smart_Chars: `<a href="./posts/smart-chars.html" class="link-white-hover">Smart Characters</a>`,
+        Social_Ladder: `<a href="./posts/social-status.html" class="link-white-hover">Social Status</a>`
+    }
+
+    const tableContents = [
+        { id: 1, link: posts.Intelligence, date:"2025-10-20", tags: ["Analysis", "INCOMPLETE"]},
+        { id: 2, link: posts.Smart_Chars, date:"2025-10-22", tags: ["Hot Take", "INCOMPLETE"]},
+        { id: 3, link: posts.Social_Ladder, date:"2025-11-11", tags: ["New", "INCOMPLETE"]},
+    ]
+
+    function renderTable(data) {
+        displayTable.innerHTML = data.map(row => `
+            <tr>
+                <td>${row.id}</td>
+                <td>${row.link}</td>
+                <td>${row.date}</td>
+                <td>${row.related || undefined}</td>
+                <td>${row.tags.join(", ")}</td>
+            </tr>
+        `).join("");
+    }
+
+    // init render
+    let display = reverseOrderButtonClicked ? tableContents : [...tableContents].reverse();
+    renderTable(display)
+
+    // mouse clicks
+    orderContentButton.addEventListener("click", () => {
+        reverseOrderButtonClicked = !reverseOrderButtonClicked
+        let display = reverseOrderButtonClicked ? tableContents : [...tableContents].reverse();
+        renderTable(display)
+    });
+
+    // search filter
+    searchInput.addEventListener('input', function() {
+        const filter = searchInput.value.toLowerCase()
+        const rows = document.querySelectorAll("#displayTable tbody tr")
+        rows.forEach(row=> {
+            const text = row.textContent.toLocaleLowerCase();
+            row.classList.toggle('hidden', !text.includes(filter))
+        })
+    });
 });
