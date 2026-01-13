29468153e8d417b86e8232fabba3f7931fec920e
Unos <129419169+shoc71@users.noreply.github.com>
2026-01-13 11:23:02 -0500

    Add 'Evil' entry to pages list

--- src/pages.js
+++ src/pages.js
@@ -3,4 +3,5 @@ export const postData = [
   { name: "Smart Characters", date: "2025-11-11", tags: ["Hot Take", "INCOMPLETE"] },
   { name: "Social Status", date: "2025-11-11", tags: ["New", "INCOMPLETE"] },
   { name: "Power Systems", date: "2025-12-20", tags: ["Power", "INCOMPLETE"] },
+  { name: "Evil", date: "2026-01-10", tags: ["Power", "INCOMPLETE"] },
 ];
